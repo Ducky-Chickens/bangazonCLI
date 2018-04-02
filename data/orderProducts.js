eae5606a609c88c80ecf7059a8437890323d16bd
Elijah Wilson <elijah.b.wilson@gmail.com>
2018-04-02 13:27:25 -0500

    migrated data from ducky chickens api into exported objects

---
+++ data/orderProducts.js
@@ -0,0 +1,83 @@
+
+module.exports.order_products = [
+  {
+    orderId: 17,
+    productId: 30
+  },
+  {
+    orderId: 16,
+    productId: 34
+  },
+  {
+    orderId: 19,
+    productId: 33
+  },
+  {
+    orderId: 10,
+    productId: 17
+  },
+  {
+    orderId: 1,
+    productId: 47
+  },
+  {
+    orderId: 12,
+    productId: 11
+  },
+  {
+    orderId: 16,
+    productId: 14
+  },
+  {
+    orderId: 15,
+    productId: 28
+  },
+  {
+    orderId: 14,
+    productId: 9
+  },
+  {
+    orderId: 3,
+    productId: 2
+  },
+  {
+    orderId: 13,
+    productId: 9
+  },
+  {
+    orderId: 16,
+    productId: 21
+  },
+  {
+    orderId: 14,
+    productId: 31
+  },
+  {
+    orderId: 14,
+    productId: 33
+  },
+  {
+    orderId: 5,
+    productId: 41
+  },
+  {
+    orderId: 20,
+    productId: 30
+  },
+  {
+    orderId: 3,
+    productId: 10
+  },
+  {
+    orderId: 4,
+    productId: 26
+  },
+  {
+    orderId: 2,
+    productId: 41
+  },
+  {
+    orderId: 18,
+    productId: 25
+  }
+]
---
+++ data/products.js
@@ -0,0 +1,355 @@
+
+module.exports.products = [
+  {
+    title:"Awesome Soft Cheese",
+    price:"25.00",
+    productTypeId:5,
+    customerId:19,
+    description:"Autem optio earum non sint non atque quos voluptatem.", 
+  },
+  {
+    title:"Small Wooden Cheese",
+    price:"341.00",
+    productTypeId:11,
+    customerId:14,
+    description:"Hic dolorem provident ipsa dolor qui."
+  },
+  {
+    title:"Awesome Metal Shirt",
+    price:"152.00",
+    productTypeId:6,
+    customerId:3,
+    description:"Qui et et placeat dolores qui dolorum distinctio odit."
+  },
+  {
+    title:"Incredible Metal Car",
+    price:"695.00",
+    productTypeId:1,
+    customerId:13,
+    description:"Rerum molestiae eius optio suscipit."
+  },
+  {
+    title:"Small Fresh Chair",
+    price:"780.00",
+    productTypeId:11,
+    customerId:7,
+    description:"Quae porro sunt id voluptas odit voluptatem aut consequatur."
+  },
+  {
+    title:"Handcrafted Wooden Keyboard",
+    price:"645.00",
+    productTypeId:8,
+    customerId:2,
+    description:"Eaque consequatur sit beatae voluptatem qui ut."
+  },
+  {
+    title:"Ergonomic Wooden Pants",
+    price:"953.00",
+    productTypeId:10,
+    customerId:11,
+    description:"Qui quibusdam maiores officia quia facilis."
+  },
+  {
+    title:"Unbranded Plastic Car",
+    price:"640.00",
+    productTypeId:11,
+    customerId:17,
+    description:"Voluptas nesciunt rem repellat et."
+  },
+  {
+    title:"Ergonomic Frozen Chicken",
+    price:"126.00",
+    productTypeId:6,
+    customerId:8,
+    description:"Saepe dolorem distinctio sit soluta illum natus possimus."
+  },
+  {
+    title:"Handmade Granite Ball",
+    price:"889.00",
+    productTypeId:6,
+    customerId:4,
+    description:"Unde fugiat consectetur et eos hic."
+  },
+  {
+    title:"Refined Fresh Pizza",
+    price:"650.00",
+    productTypeId:2,
+    customerId:14,
+    description:"Explicabo occaecati sed tenetur quas."
+  },
+  {
+    title:"Incredible Rubber Car",
+    price:"790.00",
+    productTypeId:11,
+    customerId:10,
+    description:"Sunt deserunt sint repellat sint molestiae rerum animi est quae."
+  },
+  {
+    title:"Gorgeous Soft Keyboard",
+    price:"152.00",
+    productTypeId:2,
+    customerId:17,
+    description:"Et voluptates est exercitationem nam sit id dignissimos."
+  },
+  {
+    title:"Rustic Frozen Hat",
+    price:"263.00",
+    productTypeId:4,
+    customerId:12,
+    description:"Quam aut incidunt labore ut ut natus."
+  },
+  {
+    title:"Practical Cotton Chair",
+    price:"263.00",
+    productTypeId:2,
+    customerId:4,
+    description:"Quo eos iusto qui sequi nemo est voluptatem."
+  },
+  {
+    title:"Awesome Fresh Salad",
+    price:"663.00",
+    productTypeId:6,
+    customerId:19,
+    description:"Quo ut dolorum quis error ipsa a sunt."
+  },
+  {
+    title:"Refined Fresh Tuna",
+    price:"39.00",
+    productTypeId:11,
+    customerId:19,
+    description:"Voluptatem quo hic voluptatibus non atque commodi harum nam."
+  },
+  {
+    title:"Refined Wooden Soap",
+    price:"521.00",
+    productTypeId:4,
+    customerId:14,
+    description:"Quas sequi temporibus totam enim."
+  },
+  {
+    title:"Licensed Steel Tuna",
+    price:"516.00",
+    productTypeId:7,
+    customerId:6,
+    description:"Facere sed quas accusamus maiores architecto ipsum."
+  },
+  {
+    title:"Licensed Concrete Computer",
+    price:"777.00",
+    productTypeId:4,
+    customerId:16,
+    description:"Quo cum sit."
+  },
+  {
+    title:"Ergonomic Cotton Towels",
+    price:"536.00",
+    productTypeId:8,
+    customerId:7,
+    description:"Quidem assumenda eligendi nesciunt laborum."
+  },
+  {
+    title:"Practical Steel Towels",
+    price:"632.00",
+    productTypeId:7,
+    customerId:4,
+    description:"Adipisci quidem ipsa eaque ullam facilis illo."
+  },
+  {
+    title:"Unbranded Fresh Mouse",
+    price:"498.00",
+    productTypeId:6,
+    customerId:12,
+    description:"Nemo recusandae ducimus natus qui harum aut."
+  },
+  {
+    title:"Refined Wooden Chair",
+    price:"776.00",
+    productTypeId:7,
+    customerId:1,
+    description:"Minus omnis debitis rerum sed cupiditate officiis ut."
+  },
+  {
+    title:"Small Steel Pizza",
+    price:"677.00",
+    productTypeId:11,
+    customerId:20,
+    description:"Dolores est consectetur laudantium neque."
+  },
+  {
+    title:"Fantastic Wooden Shoes",
+    price:"379.00",
+    productTypeId:1,
+    customerId:13,
+    description:"Qui et consequuntur."
+  },
+  {
+    title:"Refined Metal Mouse",
+    price:"909.00",
+    productTypeId:11,
+    customerId:6,
+    description:"Accusantium nemo dolorum eius amet quaerat laudantium."
+  },
+  {
+    title:"Intelligent Frozen Chips",
+    price:"372.00",
+    productTypeId:3,
+    customerId:2,
+    description:"Voluptatem corporis ipsam suscipit qui officiis."
+  },
+  {
+    title:"Sleek Wooden Towels",
+    price:"401.00",
+    productTypeId:11,
+    customerId:9,
+    description:"Ea sint magnam consequatur non ullam maiores accusamus totam."
+  },
+  {
+    title:"Gorgeous Frozen Fish",
+    price:"671.00",
+    productTypeId:7,
+    customerId:19,
+    description:"Aut reiciendis aliquam placeat."
+  },
+  {
+    title:"Sleek Fresh Bike",
+    price:"427.00",
+    productTypeId:10,
+    customerId:19,
+    description:"Delectus fuga reiciendis perspiciatis dolorem repellat ipsum."
+  },
+  {
+    title:"Generic Granite Towels",
+    price:"169.00",
+    productTypeId:9,
+    customerId:8,
+    description:"Libero quas ea similique."
+  },
+  {
+    title:"Handcrafted Steel Sausages",
+    price:"530.00",
+    productTypeId:4,
+    customerId:3,
+    description:"Ducimus voluptas omnis debitis quibusdam sed blanditiis asperiores aut."
+  },
+  {
+    title:"Unbranded Plastic Gloves",
+    price:"404.00",
+    productTypeId:7,
+    customerId:14,
+    description:"Quia quasi dolores voluptates debitis dolorem sapiente delectus ratione."
+  },
+  {
+    title:"Practical Fresh Fish",
+    price:"882.00",
+    productTypeId:3,
+    customerId:2,
+    description:"Esse asperiores veritatis quia qui voluptas."
+  },
+  {
+    title:"Handcrafted Steel Pizza",
+    price:"492.00",
+    productTypeId:11,
+    customerId:9,
+    description:"Distinctio porro iusto occaecati."
+  },
+  {
+    title:"Small Rubber Computer",
+    price:"544.00",
+    productTypeId:6,
+    customerId:5,
+    description:"Rerum laborum quia non at."
+  },
+  {
+    title:"Fantastic Metal Keyboard",
+    price:"963.00",
+    productTypeId:9,
+    customerId:3,
+    description:"Perferendis repudiandae quo commodi."
+  },
+  {
+    title:"Rustic Metal Car",
+    price:"978.00",
+    productTypeId:2,
+    customerId:16,
+    description:"Et sapiente voluptas."
+  },
+  {
+    title:"Fantastic Plastic Soap",
+    price:"890.00",
+    productTypeId:3,
+    customerId:12,
+    description:"Voluptas illo cupiditate alias."
+  },
+  {
+    title:"Ergonomic Frozen Pants",
+    price:"867.00",
+    productTypeId:6,
+    customerId:3,
+    description:"Rem non consequuntur quis."
+  },
+  {
+    title:"Unbranded Fresh Shirt",
+    price:"401.00",
+    productTypeId:3,
+    customerId:14,
+    description:"Et consequuntur et animi."
+  },
+  {
+    title:"Gorgeous Steel Shirt",
+    price:"778.00",
+    productTypeId:2,
+    customerId:2,
+    description:"Hic voluptate officiis maxime tempora aut dolores id."
+  },
+  {
+    title:"Handmade Granite Gloves",
+    price:"7.00",
+    productTypeId:6,
+    customerId:18,
+    description:"Soluta provident dolores."
+  },
+  {
+    title:"Tasty Cotton Towels",
+    price:"154.00",
+    productTypeId:1,
+    customerId:11,
+    description:"Corporis quasi sint voluptatem atque ut reiciendis."
+  },
+  {
+    title:"Handcrafted Fresh Bacon",
+    price:"707.00",
+    productTypeId:5,
+    customerId:2,
+    description:"Expedita vero qui dolorum deserunt modi officia quod accusamus."
+  },
+  {
+    title:"Refined Soft Keyboard",
+    price:"339.00",
+    productTypeId:9,
+    customerId:4,
+    description:"Pariatur nemo consequatur ducimus est laborum sit id."
+  },
+  {
+    title:"Handmade Concrete Table",
+    price:"547.00",
+    productTypeId:9,
+    customerId:2,
+    description:"Autem veritatis ut repellendus voluptatem ea quod est."
+  },
+  {
+    title:"Awesome Rubber Chips",
+    price:"389.00",
+    productTypeId:6,
+    customerId:9,
+    description:"Dolorem porro non enim pariatur delectus."
+  },
+  {
+    title:"Practical Wooden Ball",
+    price:"601.00",
+    productTypeId:4,
+    customerId:19,
+    description:"Et facilis laboriosam debitis rerum."
+  }
+]
+
+    
