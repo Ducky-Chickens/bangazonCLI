d1fcc4ce3ee5585421b11c8489a0e04079aec632
Elijah Wilson <elijah.b.wilson@gmail.com>
2018-04-03 14:34:06 -0500

    updated data to match required parameters for CLI

--- data/paymentTypes.js
+++ data/paymentTypes.js
@@ -1,100 +1,100 @@
 module.exports.paymentTypes [
-  {
+  1{
     type: "capacitor",
     accountNumber: "55502077",
     customerId: 18
   },
-  {
+  2{
     type: "application",
     accountNumber: "62222537",
     customerId: 12
   },
-  {
+  3{
     type: "firewall",
     accountNumber: "90991716",
     customerId: 13
   },
-  {
+  4{
     type: "port",
     accountNumber: "65798336",
     customerId: 18
   },
-  {
+  5{
     type: "alarm",
     accountNumber: "14861152",
     customerId: 6
   },
-  {
+  6{
     type: "alarm",
     accountNumber: "30850973",
     customerId: 4
   },
-  {
+  7{
     type: "port",
     accountNumber: "88480322",
     customerId: 16
   },
-  {
+  8{
     type: "alarm",
     accountNumber: "28415273",
     customerId: 12
   },
-  {
+  9{
     type: "protocol",
     accountNumber: "29245599",
     customerId: 16
   },
-  {
+  10{
     type: "alarm",
     accountNumber: "82774143",
     customerId: 2
   },
-  {
+  11{
     type: "monitor",
     accountNumber: "02326016",
     customerId: 8
   },
-  {
+  12{
     type: "sensor",
     accountNumber: "94802487",
     customerId: 1
   },
-  {
+  13{
     type: "driver",
     accountNumber: "35025706",
     customerId: 12
   },
-  {
+  14{
     type: "port",
     accountNumber: "73807288",
     customerId: 6
   },
-  {
+  15{
     type: "array",
     accountNumber: "87520805",
     customerId: 12
   },
-  {
+  16{
     type: "microchip",
     accountNumber: "77676200",
     customerId: 4
   },
-  {
+  17{
     type: "microchip",
     accountNumber: "18338287",
-    customerId: 4
+    customerId: 1
   },
-  {
+  18{
     type: "driver",
     accountNumber: "20983108",
     customerId: 3
   },
-  {
+  19{
     type: "card",
     accountNumber: "34922867",
     customerId: 13
   },
-  {
+  20{
     type: "matrix",
     accountNumber: "34399279",
     customerId: 9
