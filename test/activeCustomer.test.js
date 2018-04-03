67299666550def4394dd876a6ea6c43656ad20e1
Tim A. Conner <timaconner1@gmail.com>
2018-04-03 14:42:21 -0500

    Add getCustomers isFunction test.

--- test/activeCustomer.test.js
+++ test/activeCustomer.test.js
@@ -1,7 +1,8 @@
 const { assert: { equal, deepEqual, isFunction } } = require('chai');
 const { setActiveCustomer, getActiveCustomer } = require('../app/activeCustomer');
+const getCustomer = require('../app/models/getCustomers');
 
-describe('activeCustomer script', () => {
+describe('activate customer', () => {
     it('should export a setActiveCustomer function', () => {
         isFunction(setActiveCustomer);
     });
@@ -16,12 +17,18 @@ describe('activeCustomer script', () => {
         };
         deepEqual(getActiveCustomer(), nullCustomer);
     });
-    
+
     it('should be 1 after setting it to 1', () => {
         const customerOne = {
             id: 1,
         };
         setActiveCustomer(1);
         deepEqual(getActiveCustomer(), customerOne);
+    });
+});
+
+describe('getCustomers', () => {
+    it('should export a function', () => {
+        isFunction(getCustomer);
     });
 });
