22cb912e701bdc5bd6f320fa69562a76cf424213
Tim A. Conner <timaconner1@gmail.com>
2018-04-03 16:21:17 -0500

    Add extra space before comment.

--- app/controllers/activateCustomerCtrl.js
+++ app/controllers/activateCustomerCtrl.js
@@ -3,19 +3,38 @@ const prompt = require('prompt');
 
 /**
  * @function
+ * @name generatePossibleIdRegex
+ * @returns {regegx}
+ * @param {number} customerCount 
+ */
+const generatePossibleIdRegex = customerCount => {
+
+    // Create the ids as a list seperated with the | operators 
+    // to check if the value is between 0 and the max number (customerCount).
+    // Because with only regex, a number range is hard to do dynamically.
+    const possibleIds = [];
+    for (let i = 0; i < customerCount; i++) {
+        possibleIds.push(i);
+    }
+    return new RegExp(`^(${possibleIds.join('|')})$`);
+};
+
+/**
+ * @function
  * @name promptActivateCustomer
  * @description Used to intake customer id for activate customer.
  * @returns promise with a prompt.get in it.
+ * @param - amount of customers
  */
-module.exports = () => {
-  return new Promise( (resolve, reject) => {
-    prompt.get([{
-      name: 'customerId',
-      description: 'Enter customer id.',
-      pattern: /^[0-9]+$/,
-      required: true
-    }], function(err, results) {
-      return err ? reject(err): resolve(results);
-    })
-  });
+module.exports = customerCount => {
+    return new Promise((resolve, reject) => {
+        prompt.get([{
+            name: 'customerId',
+            description: 'Enter customer id.',
+            pattern: generatePossibleIdRegex(customerCount),
+            required: true
+        }], function (err, results) {
+            return err ? reject(err) : resolve(results);
+        })
+    });
 };
