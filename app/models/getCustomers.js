ae730e7178c731d4dd62f928ae257d8f4fcbd6e7
Tim A. Conner <timaconner1@gmail.com>
2018-04-03 15:51:01 -0500

    Change location that bangazon.sqlite is located.

--- app/models/getCustomers.js
+++ app/models/getCustomers.js
@@ -3,7 +3,7 @@ const { Database } = require('sqlite3').verbose();
 const { setActiveCustomer, getActiveCustomer } = require('../activeCustomer');
 const path = require('path');
 
-const db = new Database(path.join(__dirname, '..', 'db', 'bangazon.sqlite'));
+const db = new Database(path.join(__dirname, '..', 'bangazon.sqlite'));
 
 /**
  * @function
