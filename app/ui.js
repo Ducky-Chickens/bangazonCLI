5b284a5ffc5881e6265023397caf412e29e2b446
Tim A. Conner <timaconner1@gmail.com>
2018-04-03 15:31:01 -0500

    Add activateCustomer example schema to ui.

--- app/ui.js
+++ app/ui.js
@@ -1,35 +1,57 @@
 'use strict';
 
 // 3rd party libs
-const {red, magenta, blue} = require("chalk");
+const { red, magenta, blue } = require("chalk");
 const prompt = require('prompt');
 const colors = require("colors/safe");
 const path = require('path');
 const { Database } = require('sqlite3').verbose();
 prompt.message = colors.blue("Bangazon Corp");
 
-// app modules
+// controllers
 const { promptNewCustomer } = require('./controllers/customerCtrl')
+const promptActivateCustomer = require('./controllers/activateCustomerCtrl')
+
+// models
+const getCustomers = require('./models/getCustomers');
+
+// activeCustomer
+const { setActiveCustomer, getActiveCustomer } = require('../app/activeCustomer');
 
 const db = new Database(path.join(__dirname, '..', 'db', 'bangazon.sqlite'));
 
 prompt.start();
 
 let mainMenuHandler = (err, userInput) => {
-  console.log("user input", userInput);
   // This could get messy quickly. Maybe a better way to parse the input?
-  if(userInput.choice == '1') {
+  if (userInput.choice == '1') {
     promptNewCustomer()
-    .then( (custData) => {
-      console.log('customer data to save', custData );
-      //save customer to db
+      .then((custData) => {
+        console.log('customer data to save', custData);
+        //save customer to db
+      });
+  }
+
+  // Activate Customer
+  else if (userInput.choice == '2') {
+    getCustomers().then(customers => {
+
+      // TODO: Only allow numbers that exist in sql database
+      for(let customer of customers){
+        console.log(customer.id, customer.name);
+      }
+
+      promptActivateCustomer()
+        .then(customerId => {
+          setActiveCustomer(customerId);
+        });
     });
   }
 };
 
 module.exports.displayWelcome = () => {
   let headerDivider = `${magenta('*********************************************************')}`
-  return new Promise( (resolve, reject) => {
+  return new Promise((resolve, reject) => {
     console.log(`
   ${headerDivider}
   ${magenta('**  Welcome to Bangazon! Command Line Ordering System  **')}
@@ -44,6 +66,6 @@ module.exports.displayWelcome = () => {
     prompt.get([{
       name: 'choice',
       description: 'Please make a selection'
-    }], mainMenuHandler );
+    }], mainMenuHandler);
   });
 };
