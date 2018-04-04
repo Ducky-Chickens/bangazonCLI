ccfd0315f8db26984a300be13158c4fd41b58ef1
Tim A. Conner <timaconner1@gmail.com>
2018-04-04 14:24:08 -0500

    Add basic outlinte of GetStaleProducts

---
+++ app/models/GetStaleProducts.js
@@ -0,0 +1,41 @@
+'use strict';
+const { Database } = require('sqlite3').verbose();
+const { setActiveCustomer, getActiveCustomer } = require('../activeCustomer');
+const path = require('path');
+const db = new Database(path.join(__dirname, '..', '..', 'bangazon.sqlite'));
+
+
+/**
+ * @function
+ * @name getStaleProducts
+ * @param {integer} customerId
+ * @description Returns a list of stale products for a given customer
+ * @returns {Array.<{product}>} An array of products.
+ */
+module.exports = customerId => {
+    return new Promise((resolve, reject) => {
+
+        // A stale product meeets any of the following criteria:
+
+        // Is stale option 1:  Has never been added to an order, and has been in the system for more than 180 days
+        const neverAddedSql = ``;
+
+
+        return db.all(
+            `SELECT customer_id AS id, (first_name || ' ' || last_name) AS name]
+            FROM customers`,
+            (err, products) => {
+                return err ? reject(err) : resolve(products);
+            });
+    });
+};
+
+
+
+// Is stale option 2:  Has been added to an order, 
+// but the order hasn't been completed, 
+// and the order was created more than 90 days ago
+
+// Is stale option 3:  Has been added to one, 
+// or more orders, and the order were completed, 
+// but there is remaining quantity for the product, and the product has been in the system for more than 180 days
