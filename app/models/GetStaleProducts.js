155592ae51901dad6f688d383969ae54e5ed2eb3
Tim A. Conner <timaconner1@gmail.com>
2018-04-04 14:59:03 -0500

    Add get stale products where clause #1.

--- app/models/GetStaleProducts.js
+++ app/models/GetStaleProducts.js
@@ -18,8 +18,20 @@ module.exports = customerId => {
         // A stale product meeets any of the following criteria:
 
         // Is stale option 1:  Has never been added to an order, and has been in the system for more than 180 days
-        const neverAddedSql = ``;
+        const neverAddedSql = `SELECT P.product_id, P.product_name
+        FROM products AS P
+        WHERE 
+            P.product_id NOT IN (
+                SELECT OP.product_id
+                FROM order_products AS OP
+            )
+            AND 
+            CAST(JULIANDAY('now') - JULIANDAY(REPLACE(P.listing_date, '/', '-')) as INT) >= 180;`;
 
+        // Is stale option 2:  Has been added to an order, 
+        // but the order hasn't been completed, 
+        // and the order was created more than 90 days ago
+        const addedToNotCompletedOrderSql = ``;
 
         return db.all(
             `SELECT customer_id AS id, (first_name || ' ' || last_name) AS name]
@@ -32,9 +44,6 @@ module.exports = customerId => {
 
 
 
-// Is stale option 2:  Has been added to an order, 
-// but the order hasn't been completed, 
-// and the order was created more than 90 days ago
 
 // Is stale option 3:  Has been added to one, 
 // or more orders, and the order were completed, 
