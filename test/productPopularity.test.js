99a36f4458cdbe53d75e03635dfb3227073c25e4
Tim A. Conner <timaconner1@gmail.com>
2018-04-05 11:21:50 -0500

    Add most popular product example array test.

--- test/productPopularity.test.js
+++ test/productPopularity.test.js
@@ -1,4 +1,4 @@
-'use strict'; 
+'use strict';
 const getProductPopularity = require('../app/models/GetProductPopularity');
 const { assert: { equal, deepEqual, isArray } } = require('chai');
 
@@ -7,9 +7,30 @@ describe('Product popularity', () => {
         id: 4,
     };
 
+    const mostPopularProductExample = [
+        {
+            product: "Practical Cotton Chair",
+            orders: 4,
+            purchasers: 2,
+            revevnue: 127
+        },
+        {
+            product: "Handmade Granite Ball",
+            orders: 1,
+            purchasers: 1,
+            revevnue: 25
+        }
+    ];
+
     it('should return an array', () => {
         getProductPopularity(exampleUser.id).then(products => {
             isArray(products);
+        });
+    });
+
+    it('should deep equal most popular product exampmle', () => {
+        getProductPopularity(exampleUser.id).then(products => {
+            deepEqual(products, mostPopularProductExample);
         });
     });
 });
