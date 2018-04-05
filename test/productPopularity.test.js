0a6371566c5865a65c8f0aeb43c1071011e73593
Tim A. Conner <timaconner1@gmail.com>
2018-04-05 10:41:37 -0500

    Add outline of productPoplarity test.

---
+++ test/productPopularity.test.js
@@ -0,0 +1,9 @@
+'use strict'; 
+const { getProductPopularity } = require('../app/models/GetProductPopularity');
+const { assert: { equal, deepEqual, isArray } } = require('chai');
+
+describe('Product popularity', () => {
+    it('should return an array', () => {
+
+    });
+});
