b4b87643821cc9d6b348a14ab7c7a6ca42d7d665
Elijah Wilson <elijah.b.wilson@gmail.com>
2018-04-05 11:42:14 -0500

    fixed conflicts in orders

--- data/orders.js
+++ data/orders.js
@@ -1,6 +1,5 @@
 module.exports.orders = [
   {
-<<<<<<< HEAD
     customerId: 4,
     orderDate: "2017-5-23",
     paymentTypeId: 1
@@ -8,15 +7,6 @@ module.exports.orders = [
   {
     customerId: 1,
     orderDate: "2017-7-28",
-=======
-    customerId: 18,
-    orderDate: "2017-05-23",
-    paymentTypeId: 1
-  },
-  {
-    customerId: 3,
-    orderDate: "2017-07-28",
->>>>>>> master
     paymentTypeId: null
   },
   {
@@ -25,13 +15,8 @@ module.exports.orders = [
     paymentTypeId: 3
   },
   {
-<<<<<<< HEAD
     customerId: 2,
     orderDate: "2018-3-9",
-=======
-    customerId: 10,
-    orderDate: "2018-03-09",
->>>>>>> master
     paymentTypeId: null
   },
   {
@@ -40,13 +25,8 @@ module.exports.orders = [
     paymentTypeId: 12
   },
   {
-<<<<<<< HEAD
     customerId: 3,
     orderDate: "2017-10-7",
-=======
-    customerId: 1,
-    orderDate: "2017-10-07",
->>>>>>> master
     paymentTypeId: null
   },
   {
@@ -55,7 +35,6 @@ module.exports.orders = [
     paymentTypeId: 5
   },
   {
-<<<<<<< HEAD
     customerId: 7,
     orderDate: "2017-5-23",
     paymentTypeId: 8
@@ -63,15 +42,6 @@ module.exports.orders = [
   {
     customerId: 4,
     orderDate: "2017-8-16",
-=======
-    customerId: 12,
-    orderDate: "2017-05-23",
-    paymentTypeId: 8
-  },
-  {
-    customerId: 8,
-    orderDate: "2017-08-16",
->>>>>>> master
     paymentTypeId: null
   },
   {
@@ -85,7 +55,6 @@ module.exports.orders = [
     paymentTypeId: 10
   },
   {
-<<<<<<< HEAD
     customerId: 1,
     orderDate: "2017-7-14",
     paymentTypeId: 20
@@ -128,50 +97,6 @@ module.exports.orders = [
   {
     customerId: 3,
     orderDate: "2017-4-26",
-=======
-    customerId: 9,
-    orderDate: "2017-07-14",
-    paymentTypeId: 20
-  },
-  {
-    customerId: 12,
-    orderDate: "2017-04-19",
-    paymentTypeId: 15
-  },
-  {
-    customerId: 8,
-    orderDate: "2018-02-12",
-    paymentTypeId: null
-  },
-  {
-    customerId: 1,
-    orderDate: "2018-02-14",
-    paymentTypeId: 17
-  },
-  {
-    customerId: 6,
-    orderDate: "2017-06-30",
-    paymentTypeId: null
-  },
-  {
-    customerId: 12,
-    orderDate: "2018-03-18",
-    paymentTypeId: 15
-  },
-  {
-    customerId: 9,
-    orderDate: "2018-03-05",
-    paymentTypeId: 20
-  },
-  {
-    customerId: 15,
-    orderDate: "2017-05-06",
-    paymentTypeId: null
-  },
-  {
-    customerId: 9,
-    orderDate: "2017-04-26",
->>>>>>> master
     paymentTypeId: 20
   }
 ]
