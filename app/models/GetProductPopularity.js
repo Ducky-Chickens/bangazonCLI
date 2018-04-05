a9af26053d2d98b8b69d9f88b7d1e6355ce048ba
Tim A. Conner <timaconner1@gmail.com>
2018-04-05 10:40:59 -0500

    Add jsdoc documentation for getProductPopularity function.

---
+++ app/models/GetProductPopularity.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @function
+ * @name getProductPopularity
+ * @description Returns an array of the top three most-selling products for a given customer.
+ * @returns {Array.<{products}>} lastID of product table (primary key of newly added row)
+ * @param {integer} customerId
+ */
+module.exports = (customerId) => {
+
+};
