96701135e7dce991235c909d3efd62e4e86eab85
Tim A. Conner <timaconner1@gmail.com>
2018-04-05 11:39:07 -0500

    Commit to merge locally.

--- app/ui.js
+++ app/ui.js
@@ -9,6 +9,8 @@ const path = require('path');
 const { Database } = require('sqlite3').verbose();
 const db = new Database(path.join(__dirname, '..', 'bangazon.sqlite'));
 
+var ui = require('cliui')();
+
 prompt.message = colors.blue("Bangazon Corp");
 
 /*
@@ -25,6 +27,7 @@ MODELS
 const getCustomers = require('./models/getCustomers');
 const addCustomerProduct = require('./models/AddCustomerProduct');
 const { addCustomerPaymentType } = require('./models/AddPaymentType');
+const getProductPopularity = require('../app/models/GetProductPopularity');
 
 /*
 ACtiVE CUSTOMER
@@ -58,8 +61,8 @@ const mainMenuHandler = (err, { choice }) => {
 
         promptActivateCustomer(customers.length)
           .then(({ customerId }) => {
-            const customer = customers.find(({id}) => +id === +customerId);
-            
+            const customer = customers.find(({ id }) => +id === +customerId);
+
             setActiveCustomer(+customer.id, customer.name);
             displayWelcome();
           });
@@ -70,9 +73,9 @@ const mainMenuHandler = (err, { choice }) => {
     // Add Payment Type
     case 3: {
       //check if active customer
-      if(getActiveCustomer().id){
+      if (getActiveCustomer().id) {
         promptPaymentType().then((paymentData) => {
-          addCustomerPaymentType(getActiveCustomer(),paymentData);
+          addCustomerPaymentType(getActiveCustomer(), paymentData);
           displayWelcome();
         })
       } else {
@@ -81,24 +84,35 @@ const mainMenuHandler = (err, { choice }) => {
       }
       break;
     }
-  
 
-
-  case 4: {
-      if(getActiveCustomer().id){
+    case 4: {
+      if (getActiveCustomer().id) {
         promptAddCustomerProduct()
-        .then((productData) => {
-          addCustomerProduct(getActiveCustomer(), productData)
-          .then(lineNum=>{
-            console.log(`\n${blue(productData.title + ' added to line ' + lineNum.id)}`)
-            displayWelcome();
+          .then((productData) => {
+            addCustomerProduct(getActiveCustomer(), productData)
+              .then(lineNum => {
+                console.log(`\n${blue(productData.title + ' added to line ' + lineNum.id)}`)
+                displayWelcome();
+              });
           });
-        });
         break;
       } else {
         console.log(`\n${red('PLEASE SELECT A CUSTOMER (#2) THEN RETURN TO THIS COMMAND')}`);
         displayWelcome();
       }
+    }
+
+    // Get Overall Product Popularity
+    case 6: {
+      if (isActiveCustomerSet()) {
+        getProductPopularity(getActiveCustomer().id).then(products => {
+          console.log(products);
+        });
+      } else {
+        console.log(`Please choose active customer before getting theire products' popularity`);
+        displayWelcome();
+      }
+      break;
     }
 
   }
