f3b095b6b5a04e3d5dad5af1af9a6680c6cd576a
Tim A. Conner <timaconner1@gmail.com>
2018-04-05 10:45:34 -0500

    Add isArray test to getProductPopularity.

--- test/productPopularity.test.js
+++ test/productPopularity.test.js
@@ -1,9 +1,15 @@
 'use strict'; 
-const { getProductPopularity } = require('../app/models/GetProductPopularity');
+const getProductPopularity = require('../app/models/GetProductPopularity');
 const { assert: { equal, deepEqual, isArray } } = require('chai');
 
 describe('Product popularity', () => {
-    it('should return an array', () => {
+    const exampleUser = {
+        id: 4,
+    };
 
+    it('should return an array', () => {
+        getProductPopularity(exampleUser.id).then(products => {
+            isArray(products);
+        });
     });
 });
