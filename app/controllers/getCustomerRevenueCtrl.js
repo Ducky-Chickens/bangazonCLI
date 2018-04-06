d4623dcf1fe94f4e896924ac5da47789bdb07dea
Trey Mosley <iiimosley@gmail.com>
2018-04-06 14:14:54 -0500

    order products grouped into subarrays

--- app/controllers/getCustomerRevenueCtrl.js
+++ app/controllers/getCustomerRevenueCtrl.js
@@ -2,11 +2,15 @@
 
 module.exports = (revenue) => {
     // arr.filter((obj, index, array) => array.indexOf(obj) === index);
-    console.log(revenue);
+    // console.log(revenue);
     let allOrders = [...new Set(revenue.map(order=>order.order))].sort((a,b)=>a-b);
     let orderSets = [];
-    allOrders.forEach(order=>{
-
+    allOrders.forEach(orderID=>{
+        let prodsPerOrder = [];
+        revenue.forEach(prodOrder=>{
+            if(prodOrder.order === orderID) prodsPerOrder.push(prodOrder);
+        });
+        orderSets.push(prodsPerOrder);
     });
     console.log(orderSets);
 }
