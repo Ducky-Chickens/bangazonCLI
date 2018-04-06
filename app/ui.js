1d3254086b2c104a8548e75fd365005a48e0de5d
dhazlettjr <d.hazlett.jr@gmail.com>
2018-04-06 14:49:18 -0500

    wrapped code inside a promise and a new function in order to call the product list again if a selection couldnt be made

--- app/ui.js
+++ app/ui.js
@@ -1,39 +1,39 @@
-'use strict';
+'use strict'
 
 // 3rd party libs
-const { red, magenta, blue } = require("chalk");
+const {red, magenta, blue} = require('chalk')
 
-const prompt = require('prompt');
-const colors = require("colors/safe");
-const path = require('path');
-const { Database } = require('sqlite3').verbose();
-const db = new Database(path.join(__dirname, '..', 'bangazon.sqlite'));
+const prompt = require('prompt')
+const colors = require('colors/safe')
+const path = require('path')
+const {Database} = require('sqlite3').verbose()
+const db = new Database(path.join(__dirname, '..', 'bangazon.sqlite'))
 
-prompt.message = colors.blue("Bangazon Corp");
+prompt.message = colors.blue('Bangazon Corp')
 
 /*
 CONTROLLERS
 */
-const { promptNewCustomer } = require('./controllers/customerCtrl')
+const {promptNewCustomer} = require('./controllers/customerCtrl')
 const promptActivateCustomer = require('./controllers/activateCustomerCtrl')
-const addProductOrder = require('./controllers/addProductOrderCtrl');
-const {removeProductSchema} = require('./controllers/removeProductCtrl');
+const addProductOrder = require('./controllers/addProductOrderCtrl')
+const {removeProductSchema} = require('./controllers/removeProductCtrl')
 
 /*
 MODELS
 */
-const getCustomers = require('./models/getCustomers');
-const addProducts = require('./models/addProductOrder');
-const {removeProduct, getProducts, getOrders} = require('./models/removeProduct');
+const getCustomers = require('./models/getCustomers')
+const addProducts = require('./models/addProductOrder')
+const {removeProduct, getProducts, getOrders} = require('./models/removeProduct')
 
 /*
 ACtiVE CUSTOMER
 */
-const { setActiveCustomer, getActiveCustomer } = require('../app/activeCustomer');
+const {setActiveCustomer, getActiveCustomer} = require('../app/activeCustomer')
 
-prompt.start();
+prompt.start()
 
-const mainMenuHandler = (err, { choice }) => {
+const mainMenuHandler = (err, {choice}) => {
 
   switch (Number(choice)) {
 
@@ -41,10 +41,10 @@ const mainMenuHandler = (err, { choice }) => {
     case 1: {
       promptNewCustomer()
         .then((custData) => {
-          console.log('customer data to save', custData);
-          //save customer to db
-        });
-      break;
+          console.log('customer data to save', custData)
+        // save customer to db
+        })
+      break
     }
 
     // Activate Customer
@@ -52,54 +52,65 @@ const mainMenuHandler = (err, { choice }) => {
       getCustomers().then(customers => {
 
         for (let customer of customers) {
-          console.log(customer.id, customer.name);
+          console.log(customer.id, customer.name)
         }
 
         promptActivateCustomer(customers.length)
           .then(({customerId}) => {
 
             // TODO: Only allow ids that exist in sql database
-            setActiveCustomer(+customerId);
-            displayWelcome();
-          });
+            setActiveCustomer(+customerId)
+            displayWelcome()
+          })
+      })
+      break
+    }
+    // Remove product from active customer
+    case 7: {
+      let newArr
+      let hasAdded
+      let newFunction = () => {
+        return new Promise ((resolve,reject) => {
+        getProducts(getActiveCustomer().id).then(products => {
+          newArr = products
+          console.log('\n')
+          products.forEach((product) => {
+            console.log(`${product.product_id}. ${product.product_name}`)
+          })
+        })
+        removeProductSchema().then(deleteProd => {
+          let checkOrders
+          getOrders(deleteProd.id).then(orders => {
+            checkOrders = orders
+            resolve(hasAdded = false)
+            for (let i = 0; i < newArr.length; i++) {
+              if (newArr[i].customer_id == getActiveCustomer().id && deleteProd.id == newArr[i].product_id && checkOrders.length === 0) {
+                removeProduct(deleteProd.id)
+                hasAdded = true
+                console.log('You have successfully removed a product from your list')
+                displayWelcome();
+                break
+              }
+            }
+          })
+        })
       });
+      }
+      newFunction().then(data => {
+      if(data == false){
+        console.log('Please choose a product from the list:')
+        newFunction();
+      }
+    });
       break;
     }
-    //Remove product from active customer
-    case 7: {
-      let newArr;
-      getProducts(getActiveCustomer().id).then(products => {
-        newArr = products;
-        products.forEach((product) => {
-          console.log(`${product.product_id}. ${product.product_name}`)
-        });
-        removeProductSchema().then(deleteProd => {
-          let checkOrders;
-          getOrders(deleteProd.id).then(orders => { 
-            checkOrders = orders;
-              for(let i=0; i<newArr.length; i++){
-              if(newArr[i].customer_id == getActiveCustomer().id && deleteProd.id == newArr[i].product_id && checkOrders.length === 0 ){
-                removeProduct(deleteProd.id)
-                console.log('You have successfully removed a product from your list');
-                break;
-              } else {
-                console.log('please choose a product from the list');
-                
-            }
-          }
-          });
-      });
-    });
-    }
-    break;
   }
+}
 
-};
-
-const displayWelcome = () => {
-  const headerDivider = `${magenta('*********************************************************')}`
-  return new Promise((resolve, reject) => {
-    console.log(`
+  const displayWelcome = () => {
+    const headerDivider = `${magenta('*********************************************************')}`
+    return new Promise((resolve, reject) => {
+      console.log(`
   ${headerDivider}
   ${magenta('**  Welcome to Bangazon! Command Line Ordering System  **')}
   ${headerDivider}
@@ -110,14 +121,12 @@ const displayWelcome = () => {
   ${magenta('5.')} Complete an order
   ${magenta('6.')} See product popularity
   ${magenta('7.')} Remove a product
-  ${magenta('8.')} Leave Bangazon!`);
-    prompt.get([{
-      name: 'choice',
-      description: 'Please make a selection'
-    }], mainMenuHandler);
-  });
-};
+  ${magenta('8.')} Leave Bangazon!`)
+      prompt.get([{
+        name: 'choice',
+        description: 'Please make a selection'
+      }], mainMenuHandler)
+    })
+  }
 
-module.exports = {
-  displayWelcome
-};
+  module.exports = {displayWelcome};
