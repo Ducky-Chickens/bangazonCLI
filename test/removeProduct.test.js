acde601c6717909428f102eabe521964829f8ba2
dhazlettjr <d.hazlett.jr@gmail.com>
2018-04-06 15:15:19 -0500

    added testing

--- test/removeProduct.test.js
+++ test/removeProduct.test.js
@@ -1,6 +1,6 @@
-const { assert: { equal, deepEqual, isFunction, isArray, isObject } } = require('chai');
+const { assert: { equal, deepEqual, isFunction, isArray, isObject } } = require('chai')
 
-const {removeProduct, getProducts, getOrders } = require('../app/models/removeProduct');
+const {removeProduct, getProducts, getOrders } = require('../app/models/removeProduct')
 
 describe('products model', () => {
   let firstProduct = {
@@ -12,45 +12,37 @@ describe('products model', () => {
     description: 'Unde fugiat consectetur et eos hic.',
     listing_date: '2018-01-10',
     quantity: 1
-  };
+  }
 
+  describe('removeProduct', () => {
+    it('should export a removeProduct function', () => {
+      isFunction(removeProduct)
+    })
 
-describe('removeProduct', () => {
-  it('should export a removeProduct function', () => {
-    isFunction(removeProduct)
+    it('should export a getProducts function', () => {
+      isFunction(getProducts)
+    })
   })
 
-  it('should export a getProducts function', () => {
-    isFunction(getProducts)
-  })
-})
+  describe('removeProduct()', () => {
 
-
-describe('removeProduct()', () => {
-
-  it('should remove a product from the database', () => {
-    return removeProduct(10)
-      .then(data => {
-        return getProducts(4)
-          .then(data => {
-            let filteredArr = data.filter(product => {
-              return product.product_id === 10
+    it('should remove a product from the database', () => {
+      return removeProduct(10)
+        .then(data => {
+          return getProducts(4)
+            .then(data => {
+              let filteredArr = data.filter(product => {
+                return product.product_id === 10
+              })
+              equal(filteredArr.length, 0)
             })
-            equal(filteredArr.length, 0)
-           })
-      })
+        })
+    })
   })
-})
 
-describe('getOrders()', () => {
-  it('should be a function', () => {
-    isFunction(getOrders)
-  })
-  // it('should be an array', () => {
-  //   return getOrders.then(data => {
-  //     isObject(data)
-      
+  describe('getOrders()', () => {
+    it('should be a function', () => {
+      isFunction(getOrders)
     })
   })
 })
-});
