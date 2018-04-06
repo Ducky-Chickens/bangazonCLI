5d8f7b013d30c58b2559b0700e95063104a85ef1
Tim A. Conner <timaconner1@gmail.com>
2018-04-06 10:24:08 -0500

    Fix stale products test.  Add return and customerid that were absent.

--- test/getStaleProducts.test.js
+++ test/getStaleProducts.test.js
@@ -4,7 +4,7 @@ const { assert: { equal, deepEqual, isArray } } = require('chai');
 
 describe('The stale products module', () => {
     it('should return an array', () => {
-        getStaleProducts().then(products => {
+        return getStaleProducts(4).then(products => {
             isArray(products);
         });
     });
