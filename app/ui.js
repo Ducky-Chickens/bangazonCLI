0dcffa96dc3b9bfd147c1ef03f31741f99908334
Elijah Wilson <elijah.b.wilson@gmail.com>
2018-04-09 11:21:43 -0500

    fixed case 8

--- app/ui.js
+++ app/ui.js
@@ -1,58 +1,82 @@
-'use strict';
+"use strict";
 
 // 3rd party libs
 const { red, magenta, blue, green } = require("chalk");
 
-const assert = require('assert');
+const assert = require("assert");
 
-const prompt = require('prompt');
+const prompt = require("prompt");
 const colors = require("colors/safe");
-const path = require('path');
-const { Database } = require('sqlite3').verbose();
-const db = new Database(path.join(__dirname, '..', 'bangazon.sqlite'));
-require('console.table');
+const path = require("path");
+const { Database } = require("sqlite3").verbose();
+const db = new Database(path.join(__dirname, "..", "bangazon.sqlite"));
+require("console.table");
 
 prompt.message = colors.blue("Bangazon Corp");
 
 /*
   CONTROLLERS
 */
-const promptAddCustomer = require('./controllers/addCustomerCtrl');
-const { generatePaymentOptions, promptCompleteOrder, paymentTypeSchema } = require('./controllers/completeOrderCtrl');
-const { promptPaymentType } = require('./controllers/addPaymentTypeCtrl');
-const { promptChooseProduct, promptChooseAttribute, promptNewValue } = require('./controllers/updateProductCtrl');
-const promptAddCustomerProduct = require('./controllers/addCustomerProductCtrl');
-const { promptAvailableProducts } = require('./controllers/addOrderProductCtrl');
-const pressEnterToContinue = require('./controllers/pressEnterToContinue');
-const promptStaleProduct = require('./controllers/staleProductsCtrl');
-const promptActivateCustomer = require('./controllers/activeCustomerCtrl');
-
+const promptAddCustomer = require("./controllers/addCustomerCtrl");
+const {
+  generatePaymentOptions,
+  promptCompleteOrder,
+  paymentTypeSchema
+} = require("./controllers/completeOrderCtrl");
+const { promptPaymentType } = require("./controllers/addPaymentTypeCtrl");
+const {
+  promptChooseProduct,
+  promptChooseAttribute,
+  promptNewValue
+} = require("./controllers/updateProductCtrl");
+const promptAddCustomerProduct = require("./controllers/addCustomerProductCtrl");
+const {
+  promptAvailableProducts
+} = require("./controllers/addOrderProductCtrl");
+const pressEnterToContinue = require("./controllers/pressEnterToContinue");
+const promptStaleProduct = require("./controllers/staleProductsCtrl");
+const promptActivateCustomer = require("./controllers/activeCustomerCtrl");
 
 /*
   MODELS
 */
-const { checkForOrder, getCustomerPaymentTypes, sumOrderTotal, checkForProducts, checkProductQuantity, updateProductQuantity } = require('./models/completeOrder');
-const getCustomers = require('./models/GetCustomers');
-const addPaymentType = require('./models/AddPaymentType');
-const { getProductsById, updateProduct } = require('./models/UpdateProduct');
-const addCustomer = require('./models/AddCustomer');
-const addCustomerProduct = require('./models/AddCustomerProduct');
-const { addCustomerPaymentType } = require('./models/AddPaymentType');
-const getProductPopularity = require('../app/models/GetProductPopularity');
-const getStaleProducts = require('./models/GetStaleProducts');
+const {
+  checkForOrder,
+  getCustomerPaymentTypes,
+  sumOrderTotal,
+  checkForProducts,
+  checkProductQuantity,
+  updateProductQuantity
+} = require("./models/completeOrder");
+const getCustomers = require("./models/GetCustomers");
+const addPaymentType = require("./models/AddPaymentType");
+const { getProductsById, updateProduct } = require("./models/UpdateProduct");
+const addCustomer = require("./models/AddCustomer");
+const addCustomerProduct = require("./models/AddCustomerProduct");
+const { addCustomerPaymentType } = require("./models/AddPaymentType");
+const getProductPopularity = require("../app/models/GetProductPopularity");
+const getStaleProducts = require("./models/GetStaleProducts");
 
-const { getActiveOrder, getProducts, addOrderProduct, addOrder } = require('./models/AddOrderProduct');
-
+const {
+  getActiveOrder,
+  getProducts,
+  addOrderProduct,
+  addOrder
+} = require("./models/AddOrderProduct");
 
 /*
   ACTIVE CUSTOMER
 */
-const { setActiveCustomer, getActiveCustomer, isActiveCustomerSet } = require('./activeCustomer');
+const {
+  setActiveCustomer,
+  getActiveCustomer,
+  isActiveCustomerSet
+} = require("./activeCustomer");
 
 /*
   HELPERS
 */
-const addSpace = require('./helpers/addSpace');
+const addSpace = require("./helpers/addSpace");
 
 /*
   START OF CLI
@@ -60,19 +84,17 @@ const addSpace = require('./helpers/addSpace');
 prompt.start();
 
 const mainMenuHandler = (err, { choice }) => {
-
   switch (Number(choice)) {
-
     // Create Customer
     case 1: {
-      promptAddCustomer()
-        .then(custData => {
-          addCustomer(custData)
-            .then(custID => {
-              console.log(`\n${blue(custData.name + ' added to line ' + custID.id)}`)
-              displayWelcome();
-            });
+      promptAddCustomer().then(custData => {
+        addCustomer(custData).then(custID => {
+          console.log(
+            `\n${blue(custData.name + " added to line " + custID.id)}`
+          );
+          displayWelcome();
         });
+      });
       break;
     }
 
@@ -88,13 +110,15 @@ const mainMenuHandler = (err, { choice }) => {
     case 3: {
       //check if active customer
       if (getActiveCustomer().id) {
-        promptPaymentType().then((paymentData) => {
+        promptPaymentType().then(paymentData => {
           addPaymentType(getActiveCustomer(), paymentData);
-          console.log(`\n${blue(`${paymentData.payment} payment added`)}`)
+          console.log(`\n${blue(`${paymentData.payment} payment added`)}`);
           displayWelcome();
-        })
+        });
       } else {
-        console.log(`\n${red(`Please choose active customer before adding a payment`)}`);
+        console.log(
+          `\n${red(`Please choose active customer before adding a payment`)}`
+        );
         displayWelcome();
       }
       break;
@@ -103,25 +127,28 @@ const mainMenuHandler = (err, { choice }) => {
     // Update Product
     case 8: {
       if (getActiveCustomer().id) {
-        getProducts(getActiveCustomer())
-          .then(products => {
-            if (products.length < 1) {
-              console.log(`\n${red(`No current products listed for this customer`)}`);
-              displayWelcome();
-            } else {
-              promptChooseProduct(products).then(result => {
-                promptChooseAttribute(result).then(input => {
-                  promptNewValue(input).then(obj => {
-                    updateProduct(getActiveCustomer(), obj);
-                    console.log(`\n${blue(`${obj.column} updated`)}`);
-                    displayWelcome();
-                  })
-                })
-              })
-            }
-          })
+        getProductsById(getActiveCustomer()).then(products => {
+          if (products.length < 1) {
+            console.log(
+              `\n${red(`No current products listed for this customer`)}`
+            );
+            displayWelcome();
+          } else {
+            promptChooseProduct(products).then(product => {
+              promptChooseAttribute(product).then(input => {
+                promptNewValue(input).then(obj => {
+                  updateProduct(getActiveCustomer(), obj);
+                  console.log(`\n${blue(`${obj.column} updated`)}`);
+                  displayWelcome();
+                });
+              });
+            });
+          }
+        });
       } else {
-        console.log(`\n${red(`Please choose active customer before updating a product`)}`);
+        console.log(
+          `\n${red(`Please choose active customer before updating a product`)}`
+        );
         displayWelcome();
       }
       break;
@@ -129,17 +156,21 @@ const mainMenuHandler = (err, { choice }) => {
 
     case 4: {
       if (getActiveCustomer().id) {
-        promptAddCustomerProduct()
-          .then((productData) => {
-            addCustomerProduct(getActiveCustomer(), productData)
-              .then(lineNum => {
-                console.log(`\n${blue(productData.title + ' added to line ' + lineNum.id)}`)
-                displayWelcome();
-              });
+        promptAddCustomerProduct().then(productData => {
+          addCustomerProduct(getActiveCustomer(), productData).then(lineNum => {
+            console.log(
+              `\n${blue(productData.title + " added to line " + lineNum.id)}`
+            );
+            displayWelcome();
           });
+        });
         break;
       } else {
-        console.log(`\n${red('PLEASE SELECT A CUSTOMER (#2) THEN RETURN TO THIS COMMAND')}`);
+        console.log(
+          `\n${red(
+            "PLEASE SELECT A CUSTOMER (#2) THEN RETURN TO THIS COMMAND"
+          )}`
+        );
         displayWelcome();
       }
     }
@@ -148,55 +179,65 @@ const mainMenuHandler = (err, { choice }) => {
     case 5: {
       let active = getActiveCustomer().id;
       if (active === null) {
-        console.log(' Please activate a customer with the main menu');
-        displayWelcome()
+        console.log(" Please activate a customer with the main menu");
+        displayWelcome();
       } else {
-        checkForOrder(active)
-          .then(orders => {
-            if (orders.length === 0) {
-              console.log(" Please add some products to your order first. Returning to main menu.");
+        checkForOrder(active).then(orders => {
+          if (orders.length === 0) {
+            console.log(
+              " Please add some products to your order first. Returning to main menu."
+            );
+            displayWelcome();
+          } else {
+            let nullOrders = [];
+            for (let i = 0; i < orders.length; i++) {
+              if (orders[i].payment_type === null) {
+                nullOrders.push(orders[i]);
+              }
+            }
+            if (nullOrders.length === 0) {
+              console.log(
+                " Please add some products to your order first. Returning to main menu."
+              );
               displayWelcome();
             } else {
-              let nullOrders = []
-              for (let i = 0; i < orders.length; i++) {
-                if (orders[i].payment_type === null) {
-                  nullOrders.push(orders[i]);
-                }
-              }
-              if (nullOrders.length === 0) {
-                console.log(" Please add some products to your order first. Returning to main menu.")
-                displayWelcome();
-              } else {
-                nullOrders.forEach(order => {
-                  checkForProducts(order)
-                    .then(products => {
-                      if (products.length === 0) {
-                        console.log(" Please add some products to your order first. Returning to main menu.");
-                        displayWelcome();
-                      } else {
-                        sumOrderTotal(nullOrders[0].order_id)
-                          .then(total => {
-                            getCustomerPaymentTypes(active)
-                              .then(payTypes => {
-                                if (payTypes.length === 0) {
-                                  console.log(' Customer must have valid payment type saved to complete an order');
-                                  displayWelcome();
-                                } else {
-                                  let pattern = generatePaymentOptions(payTypes);
-                                  promptCompleteOrder(total.total, pattern, payTypes, active, products, nullOrders)
-                                    .then(result => {
-                                      displayWelcome();
-                                    });
-                                }
-                              });
+              nullOrders.forEach(order => {
+                checkForProducts(order).then(products => {
+                  if (products.length === 0) {
+                    console.log(
+                      " Please add some products to your order first. Returning to main menu."
+                    );
+                    displayWelcome();
+                  } else {
+                    sumOrderTotal(nullOrders[0].order_id).then(total => {
+                      getCustomerPaymentTypes(active).then(payTypes => {
+                        if (payTypes.length === 0) {
+                          console.log(
+                            " Customer must have valid payment type saved to complete an order"
+                          );
+                          displayWelcome();
+                        } else {
+                          let pattern = generatePaymentOptions(payTypes);
+                          promptCompleteOrder(
+                            total.total,
+                            pattern,
+                            payTypes,
+                            active,
+                            products,
+                            nullOrders
+                          ).then(result => {
+                            displayWelcome();
                           });
-                      };
+                        }
+                      });
                     });
-                })
-              }
-            };
-          });
-      };
+                  }
+                });
+              });
+            }
+          }
+        });
+      }
       break;
     }
 
@@ -210,7 +251,9 @@ const mainMenuHandler = (err, { choice }) => {
           });
         });
       } else {
-        console.log(`Please choose active customer before getting their products' popularity`);
+        console.log(
+          `Please choose active customer before getting their products' popularity`
+        );
         displayWelcome();
       }
       break;
@@ -225,61 +268,78 @@ const mainMenuHandler = (err, { choice }) => {
 
     // Add product to order
     case 10: {
-      if(isActiveCustomerSet()) {
+      if (isActiveCustomerSet()) {
         const userId = getActiveCustomer().id;
         getProducts(userId).then(products => {
           promptAvailableProducts(products).then(product => {
-            if(product) {
+            if (product) {
               getActiveOrder(userId).then(order => {
-                if(order){
-                  addOrderProduct(userId, {"orderId":order.order_id, "prodId":product.product_id, "price": product.price});
+                if (order) {
+                  addOrderProduct(userId, {
+                    orderId: order.order_id,
+                    prodId: product.product_id,
+                    price: product.price
+                  });
                   console.log(`\n${blue(`Product added to order`)}`);
                   displayWelcome();
                 } else {
                   addOrder(userId).then(newOrder => {
-                    addOrderProduct(userId, { "orderId": newOrder.id, "prodId": product.product_id, "price": product.price });
+                    addOrderProduct(userId, {
+                      orderId: newOrder.id,
+                      prodId: product.product_id,
+                      price: product.price
+                    });
                     console.log(`\n${blue(`Product added to order`)}`);
                     displayWelcome();
-                  })
+                  });
                 }
-              })
+              });
             } else {
               displayWelcome();
             }
-          })
-        })
+          });
+        });
       } else {
-        console.log(' Please choose active customer before adding to an order');
+        console.log(" Please choose active customer before adding to an order");
         displayWelcome();
       }
     }
   }
-}
+};
 
 const displayWelcome = () => {
-  const headerDivider = `${magenta('*********************************************************')}`
+  const headerDivider = `${magenta(
+    "*********************************************************"
+  )}`;
   return new Promise((resolve, reject) => {
     console.log(`
   ${headerDivider}
-  ${magenta('**  Welcome to Bangazon! Command Line Ordering System  **')}
+  ${magenta("**  Welcome to Bangazon! Command Line Ordering System  **")}
   ${headerDivider}
-  ${magenta('-- Active Customer:')} ${isActiveCustomerSet() ? getActiveCustomer().fullName : `None`}
+  ${magenta("-- Active Customer:")} ${
+      isActiveCustomerSet() ? getActiveCustomer().fullName : `None`
+    }
   ${headerDivider}
-  ${magenta('1.')} Create a customer account
-  ${magenta('2.')} Choose active customer
-  ${magenta('3.')} Create a payment option
-  ${magenta('4.')} Add product to inventory
-  ${magenta('5.')} Complete an order
-  ${magenta('6.')} See product popularity
-  ${magenta('7.')} View stale products
-  ${magenta('8.')} Update a product
-  ${magenta('9.')} Remove a product
-  ${magenta('10.')} Add to cart
-  ${magenta('11.')} Leave Bangazon!`);
-    prompt.get([{
-      name: 'choice',
-      description: 'Please make a selection'
-    }], mainMenuHandler);
+  ${magenta("1.")} Create a customer account
+  ${magenta("2.")} Choose active customer
+  ${magenta("3.")} Create a payment option
+  ${magenta("4.")} Add product to inventory
+  ${magenta("5.")} Complete an order
+  ${magenta("6.")} See product popularity
+  ${magenta("7.")} View stale products
+  ${magenta("8.")} Update a product
+  ${magenta("9.")} Remove a product
+  ${magenta("10.")} Add to cart
+  ${magenta("11.")} Leave Bangazon!`);
+    prompt.get(
+      [
+        {
+          name: "choice",
+          description: "Please make a selection"
+        }
+      ],
+      mainMenuHandler
+    );
   });
 };
 
